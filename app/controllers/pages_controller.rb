3ab8242a23c93fd6fb6b6ad602ea47577805db7a
Michael Chen <michael.chen@berkeley.edu>
2015-03-04 18:29:00 -0800

    Models

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,12 @@ class PagesController < ActionController::Base
 
 
 	def home
-		@users = User.find_by_sql(%Q{select * from users})
-		@cats = Cat.find_by_sql(%Q{select * from cats})
-		@todos = Todo.find_by_sql(%Q{select * from todos})
+		# @users = User.find_by_sql(%Q{select * from users})
+		# @cats = Cat.find_by_sql(%Q{select * from cats})
+		# @todos = Todo.find_by_sql(%Q{select * from todos})
+		@users = User.all
+		@cats = Cat.all
+		@todos = Todo.all
 
 	end
 
